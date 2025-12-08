bb2ce9f8f3d7f978d17648d9bc0db55c048e8db0
Gunnar Arctaedius <gunnar@arctaedius.com>
2025-12-08 18:23:26 +0100

    Tried to add an animation player to control the attack animation but it's too much work

--- game/entities/player/player.gd
+++ game/entities/player/player.gd
@@ -2,7 +2,20 @@ class_name Player extends CharacterBody2D
 
 @export var speed = 300.0
 
+var attacking: bool = false
+	
+
 func get_input():
+	if attacking:
+		return
+		
+	if Input.is_action_just_pressed("attack"):
+		print("attackign")
+		attacking = true
+		%AnimationPlayer.play("attack_up")
+		
+		return
+	
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	if input_direction.length() > 0:
@@ -26,3 +39,7 @@ func get_input():
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+
+
+func _on_animation_player_current_animation_changed(_name):
+	attacking = false
