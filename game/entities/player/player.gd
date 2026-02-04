822cea95a3739468a1442f38e5b952a27efbfaf0
Gunnar Arctaedius <gunnar@arctaedius.com>
2026-02-04 10:36:13 +0100

    Added a shader that moves the player and make it flash red when taking damage

--- game/entities/player/player.gd
+++ game/entities/player/player.gd
@@ -1,11 +1,17 @@
 class_name Player extends CharacterBody2D
 
+@export_group("Settings")
 @export var speed: float = 100.0
 @export var hitboxes: PlayerHitboxes
 @export var max_health: int = 12
 
+@export_group("Resources")
+@export var damage_shader: Material
+
 var attacking: bool = false
 var direction: String
+
+var animated_sprite: AnimatedSprite2D
 
 signal health_changed(new_health: int)
 var current_health: int:
@@ -19,17 +25,41 @@ func _ready():
 	%Movement.connect_to_player(self, %AnimatedSprite2D)	
 	%Attack.connect_to_player(self, %AnimatedSprite2D)	
 	
+	animated_sprite = %AnimatedSprite2D
+	invincibility_timer = $InvincibilityTimer
+	
 	current_health = max_health
 	
 func teleport(pos: Vector2):
 	velocity = Vector2.ZERO
 	global_position = pos
 	
+var invincibility_timer: Timer
+var can_take_damage: bool = true
 func take_damage(amount: int):
+	if not can_take_damage:
+		return
+		
 	current_health -= amount
-	#TODO: play animation here
+	
 	if current_health <= 0:
 		Global.game_over(GameState.LastGameStates.LOSS)
+		return
+		
+	%AnimatedSprite2D.material = damage_shader
+	can_take_damage = false
+	invincibility_timer.start()
+	await invincibility_timer.timeout
+	%AnimatedSprite2D.material = Material.new()
+	can_take_damage = true
+
+var _elapsed_time: float = 0
+func _process(delta):
+	if animated_sprite.material == damage_shader:
+		animated_sprite.material.set_shader_parameter("elapsed", _elapsed_time)
+		_elapsed_time += delta
+	else:
+		_elapsed_time = 0
 	
 func _physics_process(_delta):
 	if not attacking:
