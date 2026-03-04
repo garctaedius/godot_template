9dafb98e7d7afa17a1f2bcdd1908d95a723c34f9
Gunnar Arctaedius <gunnar@arctaedius.com>
2026-03-04 13:35:15 +0100

    Added footsteps

--- game/entities/player/movement.gd
+++ game/entities/player/movement.gd
@@ -3,6 +3,8 @@ extends Node
 var player: Player
 var animated_sprite: AnimatedSprite2D
 
+@export var step_sound_cooldown: float
+
 func connect_to_player(_player: Player, _animated_sprite: AnimatedSprite2D):
 	player = _player
 	animated_sprite = _animated_sprite
@@ -17,12 +19,32 @@ func handle_movement():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	if input_direction.length() > 0:
-		direction = Utils.get_cardinal_direction(input_direction)
-			
-		player.direction = direction
-		animated_sprite.play(Utils.direction_to_string(direction))
-		movement = input_direction * player.speed  # input_direction is normalized automatically
+		movement = move_player(input_direction)
+		
 	else:
 		animated_sprite.stop()
 	
 	player.velocity = movement
+
+func move_player(input_direction: Vector2) -> Vector2:
+	direction = Utils.get_cardinal_direction(input_direction)
+			
+	player.direction = direction
+	animated_sprite.play(Utils.direction_to_string(direction))
+	
+	play_step_sound()
+	
+	return input_direction * player.speed  # input_direction is normalized automatically
+	
+var can_play_step_sound: bool = true
+func play_step_sound():
+	if can_play_step_sound:
+		can_play_step_sound = false
+		
+		player.sfx_player.step_player.play()
+		
+		$StepSoundTimer.start(step_sound_cooldown)
+
+
+func _on_step_sound_timer_timeout():
+	can_play_step_sound = true
--- utility/default_audio_settings.gd
+++ utility/default_audio_settings.gd
@@ -1,8 +1,8 @@
 extends Node
 
-@export var master: int
-@export var music: int
-@export var sfx: int
+@export var master: int = 70
+@export var music: int = 40
+@export var sfx: int = 70
 
 @onready var sound_settings: Dictionary[String, float] = {
 	"Master": master,
